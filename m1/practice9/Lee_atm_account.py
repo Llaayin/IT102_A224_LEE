57ac733b9f263fa394a11289a64785eb17b42ee8
Aaron Jared Lee <aaronjaredlee110606@gmail.com>
2026-09-13 13:30:23 +0800

    Update Lee_atm_account.py

--- m1/practice9/Lee_atm_account.py
+++ m1/practice9/Lee_atm_account.py
@@ -1,5 +1,5 @@
 class Account:
-
+    
     def __init__(self, name, starting_balance):
         self.account_name = name
         self._balance = starting_balance
@@ -9,18 +9,21 @@ class Account:
 
     def deposit(self, amount):
         if amount > 0:
-            self._balance += amount
+            self._balance = self._balance + amount
             return True
-        return False
+        else:
+            return False
 
     def withdraw(self, amount):
         if amount > 0 and amount <= self._balance:
-            self._balance -= amount
+            self._balance = self._balance - amount
             return True
-        return False
+        else:
+            return False
+
 
 if __name__ == "__main__":
-    account = Account("Lee", 10000.00)
+    account = Account("Juan Dela Cruz", 10000.00)
     print(f"Starting balance: ₱{account.check_balance():,.2f}")
     
     result = account.withdraw(2000.00)
